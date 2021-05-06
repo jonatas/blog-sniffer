cf3a4bff98da6f987f04afc505a42a3d0bbcbaa3
Jônatas Davi Paganini <jonatasdp@gmail.com>
2021-05-06 20:45:08 -0300

    Decrease timeout to not loose time with slow websites

--- lib/blog/agent.rb
+++ lib/blog/agent.rb
@@ -7,6 +7,7 @@ module Blog
     end
     def instance
       @agent ||= Mechanize.new { |a|
+        a.open_timeout = a.read_timeout = 1
         a.post_connect_hooks << lambda { |_,_,response,_|
           if response.content_type.nil? || response.content_type.empty?
             response.content_type = 'text/html'
