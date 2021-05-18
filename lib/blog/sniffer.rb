6ce0cac6f7642d05e598278dfb7730d5396968a5
Jônatas Davi Paganini <jonatasdp@gmail.com>
2021-05-17 21:29:34 -0300

    Normalize URL and remove params to avoid multiple redirections to the same domain

--- lib/blog/sniffer.rb
+++ lib/blog/sniffer.rb
@@ -14,7 +14,6 @@ module Blog
       def initialize(processor, options = {})
         @processor = processor
 
-        @results  = []
         @urls     = []
         @interval = options.fetch(:interval, REQUEST_INTERVAL)
         @max_urls = options.fetch(:max_urls, MAX_URLS)
@@ -27,23 +26,18 @@ module Blog
         @urls << url
       end
 
-      def record(data = {})
-        @results << data
-      end
 
       def results
         return enum_for(:results) unless block_given?
 
-        i = @results.length
         enqueued_urls.each do |url|
           begin
             log "Handling", url.inspect
-            @processor.process(url) if block_given?
-            yield @results.last
-            i += 1
+
+            yield @processor.process(url) if block_given?
           end
         rescue => ex
-          log "Error", "#{url.inspect}, #{ex}"
+          log "Error fetching #{url}", ex
         end
       end
 
@@ -88,31 +82,40 @@ module Blog
       end
 
       def process(url)
-        page, extra_payload  = fetch(url)
+        page, extra_payload = fetch(url)
         metadata = metadata_from(page).merge(extra_payload)
-
-        spider.record(metadata)
 
         page.links_with(href: /^\/|^#{Regexp.escape(@root)}/).each do |link|
           next if link.href =~ /\.(mp4|pdf|png|docx|jpg|jpeg)$/
-          spider.enqueue(link.href)
+          spider.enqueue(build_url(link.href))
         end
+        metadata
       end
 
       def build_url url
-        case url
+        case url.to_s
         when '/' then @root
-        when /^https?:\/\// then url
-        else @root + url
+        when /^https?:\/\// then normalize_url(url.to_s)
+        else normalize_url(@root + url)
         end
+      end
+
+      def normalize_url url
+        uri = URI.parse(url.to_s)
+        uri.normalize!
+        uri.query = nil
+        URI.encode(uri.to_s.gsub(/[\?#]+$/,''))
+      rescue
+        url.split("?").first
       end
 
       def metadata_from(page)
         {
           title: normalize(page.title).first,
           headers: normalize(*page.search("h1, h2, h3, h4").map(&:text)),
-          links: page.links.each_with_object({}) {|link, resume| resume[normalize(link.text)] = link.href},
-          codeblocks: normalize(*page.search(".ts-code-block").map(&:text)),
+          links: page.links.each_with_object({}) {|link, resume|
+            resume[normalize(link.text).first] = normalize_url(link.href)},
+          codeblocks: normalize(*page.search("pre").map(&:text)),
           body: normalize(*page.search("p, li").map(&:text).uniq),
           html_size: page.body.size,
         }
